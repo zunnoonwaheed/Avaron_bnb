fb666f13982551257bf9492c17670c04beefe006
Spiderman <spidey@Spidermans-MacBook-Pro.local>
2026-07-07 21:35:20 +0500

    changed the details of the newly added testimonials

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1293,8 +1293,8 @@ const Index = () => {
                     <Star key={k} className="h-4 w-4 fill-current" />
                   ))}
                 </div>
-                <p className="mt-3 text-base font-bold">AvaronBnB Owner</p>
-                <p className="text-sm opacity-80">Video testimonial</p>
+                <p className="mt-3 text-base font-bold"> Abdullah Aslam</p>
+                <p className="text-sm opacity-80">Texas, USA</p>
               </div>
               <button
                 onClick={() => setVideo4Muted(!video4Muted)}
@@ -1327,8 +1327,8 @@ const Index = () => {
                     <Star key={k} className="h-4 w-4 fill-current" />
                   ))}
                 </div>
-                <p className="mt-3 text-base font-bold">AvaronBnB Owner</p>
-                <p className="text-sm opacity-80">Video testimonial</p>
+                <p className="mt-3 text-base font-bold">Saad</p>
+                <p className="text-sm opacity-80">Portugal</p>
               </div>
               <button
                 onClick={() => setVideo5Muted(!video5Muted)}
